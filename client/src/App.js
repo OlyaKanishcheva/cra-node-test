8f8c280b39534626bcb2af4461c690df56637e10
Olga Kanishcheva <kanishcheva@phystech.edu>
2018-08-19 14:42:56 +0300

    🎉

--- client/src/App.js
+++ client/src/App.js
@@ -11,9 +11,9 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          💃💃💃💃💃💃
           <br />
-          Go go! 
+          🕺🏻🕺🏻🕺🏻🕺🏻🕺🏻🕺🏻
         </p>
       </div>
     );
